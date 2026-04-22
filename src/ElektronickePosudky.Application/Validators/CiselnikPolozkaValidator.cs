16b06ff0afd53407c779efd8be3b7b7d8e4bb132
Kien Nguyen <kien.nguyen@eastgate-software.com>
2026-04-22 11:45:07 +0700

    validate all the codelists in the request

---
+++ src/ElektronickePosudky.Application/Validators/CiselnikPolozkaValidator.cs
@@ -0,0 +1,25 @@
+﻿using ElektronickePosudky.Application.DTO;
+using ElektronickePosudky.Application.Interfaces;
+using FluentValidation;
+
+namespace ElektronickePosudky.Application.Validators
+{
+    public sealed class CiselnikPolozkaValidator
+        : AbstractValidator<PosudekRoCiselnikPolozkaCreateDto?>
+    {
+        private IElektronickePosudkyContext _context;
+
+        public CiselnikPolozkaValidator(IElektronickePosudkyContext context)
+        {
+            _context = context;
+            RuleFor(x => x)
+                .NotNull()
+                .Must(x =>
+                    _context.CiselnikPolozky.Any(e => e.Kod == x!.Kod && e.Verze == x!.Verze)
+                )
+                .WithMessage(x =>
+                    $"Not found codelist (kod: {x!.Kod}, verze: {x!.Verze}) in the database"
+                );
+        }
+    }
+}
--- tests/ElektronickePosudky.Tests/Unit/CreatePosudekValidatorTests.cs
+++ tests/ElektronickePosudky.Tests/Unit/CreatePosudekValidatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using ElektronickePosudky.Application.Commands;
 using ElektronickePosudky.Application.DTO;
 using ElektronickePosudky.Application.Validators;
+using ElektronickePosudky.Infrastructure.Utils;
 using FluentAssertions;
 using Xunit;
 
@@ -11,27 +12,62 @@ namespace ElektronickePosudky.Tests.Unit
         [Fact]
         public void Validate_ReturnsValidResultForValidCommand()
         {
-            var validator = new CreatePosudekValidator();
+            using var context = TestHelpers.CreateContext();
+            var validator = new CreatePosudekValidator(context);
             var command = new CreatePosudekCommand
             {
                 Posudek = new PosudekRoCreateDto
                 {
-                    Rid = System.Guid.NewGuid(),
-                    KrzpId = System.Guid.NewGuid(),
+                    Rid = GuidUtils.Generate(GuidUtils.PacientPrefix, 1),
+                    KrzpId = GuidUtils.Generate(GuidUtils.ZdravotnickyPracovnikPrefix, 1),
                     DatumVystaveni = System.DateTime.UtcNow,
-                    TypAkce = new PosudekRoCiselnikPolozkaCreateDto(),
-                    StavPosudku = new PosudekRoCiselnikPolozkaCreateDto(),
-                    DruhProhlidky = new PosudekRoCiselnikPolozkaCreateDto(),
-                    DruhPosudku = new PosudekRoCiselnikPolozkaCreateDto(),
-                    Zpusobilosti = new[]
+                    TypAkce = new PosudekRoCiselnikPolozkaCreateDto
                     {
+                        Kod = "akce_ro_1",
+                        Verze = "1.0.0",
+                    },
+                    StavPosudku = new PosudekRoCiselnikPolozkaCreateDto
+                    {
+                        Kod = "stav_posudku_2",
+                        Verze = "1.0.0",
+                    },
+                    DruhProhlidky = new PosudekRoCiselnikPolozkaCreateDto
+                    {
+                        Kod = "druh_prohlidky_ro_1",
+                        Verze = "1.0.0",
+                    },
+                    DruhPosudku = new PosudekRoCiselnikPolozkaCreateDto
+                    {
+                        Kod = "druh_posudku_ro_1",
+                        Verze = "1.0.0",
+                    },
+                    Zpusobilosti =
+                    [
                         new PosudekRoZpusobilostCreateDto
                         {
-                            SkupinaZadateleRidic = new PosudekRoCiselnikPolozkaCreateDto(),
-                            Vysledek = new PosudekRoCiselnikPolozkaCreateDto(),
-                            SkupinyRidicskehoOpravneni = new[] { new PosudekRoSkupinaCreateDto() },
+                            SkupinaZadateleRidic = new PosudekRoCiselnikPolozkaCreateDto
+                            {
+                                Kod = "skupina_ro_1",
+                                Verze = "1.0.0",
+                            },
+                            Vysledek = new PosudekRoCiselnikPolozkaCreateDto
+                            {
+                                Kod = "vysledek_posudku_ro_1",
+                                Verze = "1.0.0",
+                            },
+                            SkupinyRidicskehoOpravneni =
+                            [
+                                new PosudekRoSkupinaCreateDto
+                                {
+                                    SkupinaRo = new PosudekRoCiselnikPolozkaCreateDto
+                                    {
+                                        Kod = "A",
+                                        Verze = "1.0.0",
+                                    },
+                                },
+                            ],
                         },
-                    },
+                    ],
                 },
             };
 
@@ -43,7 +79,8 @@ namespace ElektronickePosudky.Tests.Unit
         [Fact]
         public void Validate_ReturnsInvalidResultWhenRequiredFieldsAreMissing()
         {
-            var validator = new CreatePosudekValidator();
+            using var context = TestHelpers.CreateContext();
+            var validator = new CreatePosudekValidator(context);
             var command = new CreatePosudekCommand();
 
             var result = validator.Validate(command);
